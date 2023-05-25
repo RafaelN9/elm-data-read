0031e7351551572daf8471a7f86d41bf2724d19a
Rafael Correia <raphael@buildaz.io>
2023-05-25 09:38:52 -0300

    Salvando dados para um CSV e mostrando PIDs corretos

--- README.md
+++ README.md
@@ -1,1 +1,24 @@
-# elm-data-read
+# Leitura e Monitoramento de Dados de Motores de Combustão Interna
+
+## 1. Como usar?
+
+Para usar o programa, intale python e pip em sua máquina e execute o seguinte comando para instalar as dependências do projeto:
+
+```sh
+    pip install -r requirements.txt
+```
+
+Caso não tenha um dispositivo ELM e um veículo à disposição, é possível utilizar um emulador executando os comandos:
+
+```sh
+    python3 -m pip install git+https://github.com/ircama/ELM327-emulator #para instalar
+    elm #para executar o emulador
+```
+
+Durante a execução ele exibe qual porta está conectada (usando linux, a porta padrão é: "/dev/pts/2")
+
+Execute o script usando o comando:
+
+```sh
+    python obd-test.py
+```
--- obd-test.py
+++ obd-test.py
@@ -1,5 +1,7 @@
 import obd
+from obd import OBDStatus
 import time
+import csv
 
 # Define os PIDs
 pids = [
@@ -13,15 +15,35 @@ pids = [
 
 # Conecta com o adaptador OBD-II
 connection = obd.OBD("/dev/pts/2")
+if connection.status() == OBDStatus.NOT_CONNECTED:
+    print("Não foi possível conectar ao adaptador OBD-II")
+    exit()
+if connection.status() == OBDStatus.CAR_CONNECTED:
+    print("O adaptador OBD-II iniciou a conexão com o veículo!\nEntrando em modo de execução...")
+    time.sleep(3)  # Aguarda 5 segundos para o veículo responder
 
-# Leitura e exibição dos dados
-def read_and_display_data():
+# Cria uma lista para armazenar os dados
+data = []
+
+# Faz a leitura e armazenamento dos dados
+def read_and_store_data():
+    row = []
     for pid in pids:
         response = connection.query(pid)
         print(f"{pid.desc}: {response.value}")
+        row.append(response.value.magnitude)
+    data.append(row)
 
-# Leitura e exibição dos dados contínua
-while True:
-    read_and_display_data()
-    time.sleep(0.5)  # Tempo de atualização
-    print("\033c", end="")  # Limpa o terminal
+# Realiza leitura e armazenamento dos dados continuamente
+try:
+    while True:
+        read_and_store_data()
+        time.sleep(0.5)  # Intervalo de 0.5 segundos
+        print("\033c", end="")  # Limpa o terminal
+except KeyboardInterrupt:
+    # Exporta os dados para um arquivo CSV
+    with open('obd_data.csv', mode='w') as file:
+        writer = csv.writer(file)
+        writer.writerow([pid.desc for pid in pids])
+        for row in data:
+            writer.writerow(row)
