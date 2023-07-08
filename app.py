ad6225b24f4762dd3b24f91f2ded48f62da0be63
Rafael Correia <rafael_correia_ext@carrefour.com>
2023-07-08 20:29:21 -0300

    API com PIDs iniciais finalizado

--- app.py
+++ app.py
@@ -1,13 +1,13 @@
-from flask import Flask, Response, jsonify
+from flask import Flask, Response
 from obd import OBDStatus, commands, OBD
-import time
+import time, sys
 
 app = Flask(__name__)
+port = "/dev/pts/1"
 
 # Stream the data using Flask
 @app.route('/stream')
 def stream_data():
-    # Define the PIDs
     pids = [
         commands.RPM,               # 0C
         commands.SPEED,             # 0D
@@ -17,39 +17,36 @@ def stream_data():
         commands.RUN_TIME,          # 1F
     ]
 
-    # # Connect to the OBD-II adapter
-    # connection = OBD("/dev/ttys009")
-    # if connection.status() == OBDStatus.NOT_CONNECTED:
-    #     print("Failed to connect to the OBD-II adapter")
-    #     exit()
-    # if connection.status() == OBDStatus.CAR_CONNECTED:
-    #     print("The OBD-II adapter has established a connection with the vehicle!\nEntering execution mode...")
-    #     time.sleep(3)  # Wait for 5 seconds for the vehicle to respond
+    # Connect to the OBD-II adapter
+    connection = OBD(port)
+    if connection.status() == OBDStatus.NOT_CONNECTED:
+        print("Failed to connect to the OBD-II adapter")
+        exit()
+    if connection.status() == OBDStatus.CAR_CONNECTED:
+        print("The OBD-II adapter has established a connection with the vehicle!\nEntering execution mode...")
+        time.sleep(3)  # Wait for 5 seconds for the vehicle to respond
 
     def generate_data():
         i = 0
         while True:
-            # row = []
-            # for pid in pids:
-            #     response = connection.query(pid)
-            #     data = f"{pid.desc}: {response.value}"
-            #     yield data + '\n'
-            #     row.append(response.value.magnitude)
-            i += 1
-            yield f"Hello World{i}\n"
+            read = []
+            for pid in pids:
+                response = connection.query(pid)
+                data = {
+                    "PID Name": pid.desc,
+                    "unit": str(response.value.unit),
+                    "magnitude": response.value.magnitude,
+                    "string": str(response.value),
+                }
+                read.append(data)
+            read = {"read": read}
+            yield "<script> document.body.innerHTML = ''</script>"
+            yield str(read)
             time.sleep(0.5)  # 0.5-second interval
-
-    return Response(generate_data(), mimetype='text/plain')
-
-@app.route('/bar-size', methods=['GET'])
-def get_bar_size():
-    # Replace this with your logic to determine the bar size
-    bar_size = 0.1
-
-    response = {
-        'barSize': bar_size
-    }
-    return jsonify(response)
+    return Response(generate_data(), mimetype='text/html')
 
 if __name__ == '__main__':
+    # get port to use obd protocol
+    if len(sys.argv) > 1:
+        port = sys.argv[1]
     app.run(host='0.0.0.0')
